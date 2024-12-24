da9f00bc9c31b52d4d544752b3d8175594ba5c96
KYs <amitkys59@gmail.com>
2024-12-24 12:54:24 +0530

    card is added

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,28 @@
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Divider } from "@nextui-org/divider";
+
 import DefaultLayout from "@/layouts/default";
+
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <section>
-        <h1 className="text-green-500">this is heading</h1>
-      </section>
+      <div className="grid grid-cols-3">
+        <Card>
+          <CardHeader>
+            <h1>this is heading</h1>
+          </CardHeader>
+          <Divider />
+
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+          <Divider />
+
+          <CardFooter>
+            <p>this is footer</p>
+          </CardFooter>
+        </Card>
+      </div>
     </DefaultLayout>
   );
 }
