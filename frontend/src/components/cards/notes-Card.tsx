552d321f98b96ded3c933414322476d9776cf00b
KYs <amitkys59@gmail.com>
2024-12-24 18:20:11 +0530

    card is organised

---
+++ frontend/src/components/cards/notes-Card.tsx
@@ -0,0 +1,55 @@
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Divider } from "@nextui-org/divider";
+import { useDisclosure } from "@nextui-org/modal";
+import { RiEdit2Line } from "react-icons/ri";
+import { MdDelete } from "react-icons/md";
+
+import DeleteNoteModal from "@/components/cards/deleteNoteModal";
+import UpdateNoteModal from "@/components/cards/updateNoteModal";
+export default function NotesCard() {
+  // Separate disclosures for each modal
+  const updateNoteDisclosure = useDisclosure();
+  const deleteNodeDisclosure = useDisclosure();
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3">
+      <Card className="shadow-lg border border-gray-600">
+        <CardHeader>
+          <h1>This is heading</h1>
+        </CardHeader>
+        <Divider />
+
+        <CardBody>
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+        <Divider />
+
+        <CardFooter className="flex justify-end gap-2">
+          <button
+            className="bg-black rounded-full p-1 cursor-pointer"
+            onClick={updateNoteDisclosure.onOpen}
+          >
+            <RiEdit2Line className="text-primary-500 text-xl" />
+          </button>
+          <button
+            className="bg-black rounded-full p-1 cursor-pointer"
+            onClick={deleteNodeDisclosure.onOpen}
+          >
+            <MdDelete className="text-red-500 text-xl" />
+          </button>
+        </CardFooter>
+      </Card>
+
+      {/* Edit Modal */}
+      <UpdateNoteModal
+        isOpen={updateNoteDisclosure.isOpen}
+        onClose={updateNoteDisclosure.onClose}
+      />
+      {/* Delete Modal */}
+      <DeleteNoteModal
+        isOpen={deleteNodeDisclosure.isOpen}
+        onClose={deleteNodeDisclosure.onClose}
+      />
+    </div>
+  );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,11 @@
-import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
-import { useDisclosure } from "@nextui-org/modal";
-import { MdDelete } from "react-icons/md";
-import { Divider } from "@nextui-org/divider";
-import { RiEdit2Line } from "react-icons/ri";
 import { IoIosAddCircleOutline } from "react-icons/io";
+import { useDisclosure } from "@nextui-org/modal";
 
 import CreateNoteModal from "@/components/cards/createNoteModal";
-import DeleteNoteModal from "@/components/cards/deleteNoteModal";
-import UpdateNoteModal from "@/components/cards/updateNoteModal";
 import DefaultLayout from "@/layouts/default";
+import NotesCard from "@/components/cards/notes-Card";
 
 export default function IndexPage() {
-  // Separate disclosures for each modal
-  const updateNoteDisclosure = useDisclosure();
-  const deleteNodeDisclosure = useDisclosure();
   const createNoteDisclosure = useDisclosure();
 
   return (
@@ -26,54 +18,13 @@ export default function IndexPage() {
         <IoIosAddCircleOutline className="text-4xl text-green-600 lg:text-6xl" />
       </button>
       <div className="min-h-screen">
-        <div className="grid grid-cols-1 lg:grid-cols-3">
-          <Card className="shadow-lg border border-gray-600">
-            <CardHeader>
-              <h1>This is heading</h1>
-            </CardHeader>
-            <Divider />
-
-            <CardBody>
-              <p>
-                Make beautiful websites regardless of your design experience.
-              </p>
-            </CardBody>
-            <Divider />
-
-            <CardFooter className="flex justify-end gap-2">
-              <button
-                className="bg-black rounded-full p-1 cursor-pointer"
-                onClick={updateNoteDisclosure.onOpen}
-              >
-                <RiEdit2Line className="text-primary-500 text-xl" />
-              </button>
-              <button
-                className="bg-black rounded-full p-1 cursor-pointer"
-                onClick={deleteNodeDisclosure.onOpen}
-              >
-                <MdDelete className="text-red-500 text-xl" />
-              </button>
-            </CardFooter>
-          </Card>
-
-          {/* Edit Modal */}
-          <UpdateNoteModal
-            isOpen={updateNoteDisclosure.isOpen}
-            onClose={updateNoteDisclosure.onClose}
-          />
-          {/* Delete Modal */}
-          <DeleteNoteModal
-            isOpen={deleteNodeDisclosure.isOpen}
-            onClose={deleteNodeDisclosure.onClose}
-          />
-
-          {/* create post modal  */}
-          <CreateNoteModal
-            isOpen={createNoteDisclosure.isOpen}
-            onClose={createNoteDisclosure.onClose}
-          />
-        </div>
+        <NotesCard />
       </div>
+      {/* create post modal  */}
+      <CreateNoteModal
+        isOpen={createNoteDisclosure.isOpen}
+        onClose={createNoteDisclosure.onClose}
+      />
     </DefaultLayout>
   );
 }
