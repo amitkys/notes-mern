6459495af5d5354399be3e526b4bf983da560873
KYs <amitkys59@gmail.com>
2024-12-24 13:10:24 +0530

    card UI

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { MdDelete } from "react-icons/md";
 import { Divider } from "@nextui-org/divider";
+import { RiEdit2Line } from "react-icons/ri";
 
 import DefaultLayout from "@/layouts/default";
 
@@ -7,7 +9,7 @@ export default function IndexPage() {
   return (
     <DefaultLayout>
       <div className="grid grid-cols-3">
-        <Card>
+        <Card className="shadow-lg border border-gray-600">
           <CardHeader>
             <h1>this is heading</h1>
           </CardHeader>
@@ -18,8 +20,13 @@ export default function IndexPage() {
           </CardBody>
           <Divider />
 
-          <CardFooter>
-            <p>this is footer</p>
+          <CardFooter className="flex justify-end gap-2">
+            <div className="bg-black rounded-full p-1">
+              <RiEdit2Line className="text-primary-500 text-xl" />
+            </div>
+            <div className="bg-black rounded-full p-1">
+              <MdDelete className="text-red-500 text-xl" />
+            </div>
           </CardFooter>
         </Card>
       </div>
