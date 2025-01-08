fdf1bfe9d7db0d667f58cf5cb9054518ec7744de
KYs <amitkys59@gmail.com>
2025-01-08 17:37:26 +0530

    vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/notes-mern/frontend",
+  // base: "/notes-mern/frontend",
 
 })
