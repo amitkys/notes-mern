7b24c5cd89652a722bca71eb88bb50a342e626d7
KYs <amitkys59@gmail.com>
2025-01-09 15:09:52 +0530

    backend auto live code pushed

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,22 @@ app.use((err, req, res, next) => {
 mongoose.connect(process.env.DB_CONNECTION_URI)
 .then(() => {console.log('connected to db')}).catch((e) => console.log('error to connect db', e));
 
+const url = `https://notes-mern-backend-ti1q.onrender.com`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 app.get('/', (req, res) => {
     res.json({message: 'hello from backend'});
 });
